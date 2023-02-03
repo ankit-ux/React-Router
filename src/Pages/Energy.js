ca58ff7b18b5047a05d0630679a37f97efb1f873
zesmychannel <zesmychannel@gmail.com>
2023-02-03 20:38:04 +0530

    React-Router

---
+++ src/Pages/Energy.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../com/Header'
+
+const Energy = () => {
+  return (
+    <div>
+      <Header></Header>
+      Energy
+    </div>
+  )
+}
+
+export default Energy
---
+++ src/Pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Cardu from '../com/Cardu'
+import Cards from '../com/Cardu'
+import Header from '../com/Header'
+
+const Home = () => {
+  return (
+    <div>
+        <Header></Header>
+      <Cardu></Cardu>
+    </div>
+  )
+}
+
+export default Home
